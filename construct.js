9b132edebdbf22b19b20e57c516c5c33ca5b2074
RemLiquit <ceixpp@gmail.com>
2020-09-19 13:12:41 -0400

    object: funcio contructora

---
+++ construct.js
@@ -0,0 +1,9 @@
+function auto(marca, modelo, anio) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.anio = anio;
+}
+
+var autoNuevo = new auto("Tesla", "Model 3", 2020);
+var autoNuevo2 = new auto("Tesla", "Model X", 2018);
+var autoNuevo3 = new auto("Toyota", "Corola", 2010);
